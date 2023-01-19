43a83b019ca65a6035cede7ae17e8303eb27b5aa
Ankit Kanyal <ankitsinghkanyal43@gmail.com>
2023-01-19 10:55:09 +0530

    Done with scaffolding of the project.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+import "./App.css";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+
+function App() {
+  return (
+    <Home />
+    // <About/>
+  );
+}
+
+export default App;
