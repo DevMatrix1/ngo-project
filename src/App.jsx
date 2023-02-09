9973bc1c25f7079b6cf4818c426a295c6d8770d5
kotesh-arya <koteswarraomudila@gmail.com>
2023-02-09 19:41:03 +0530

    FEAT:added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
 
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 
 import MailForm from "./components/MailForm";
 // import Home from "./pages/Home/Home";
 // import About from "./pages/About/About";
+
 function App() {
   return (
     // <Home />
     // <About/>
     <MailForm />
-
+    // <Footer />
   );
 }
 
