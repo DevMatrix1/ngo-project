53cf560aeb17391b38903f92ba33de5c580ae1f5
kotesh-arya <koteswarraomudila@gmail.com>
2023-02-17 17:36:05 +0530

    REFACTOR: Imported Footer to Form Page

--- src/pages/Form/Form.jsx
+++ src/pages/Form/Form.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import Footer from "../../components/Footer/Footer";
 import MailForm from "../../components/MailForm";
 import { Navbar } from "../../components/Navbar/Navbar";
+
 function Form() {
   return (
     <div>
       <Navbar />
       <MailForm />
+      <Footer/>
     </div>
   );
 }
