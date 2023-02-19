b65516a3f4c94f1706b3329de5f16d1097eb6ded
kotesh-arya <koteswarraomudila@gmail.com>
2023-02-19 10:56:43 +0530

    REFACTOR: removed carousel import

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,11 @@ import { useState } from "react";
 
 import "./App.css";
 
-import { CarouselSlider } from "./components/Carousel/CarouselSlider";
-import { Navbar } from "./components/Navbar/Navbar";
-
-// import MailForm from "./components/MailForm";
 import Form from "./pages/Form/Form";
-// import Home from "./pages/Home/Home";
-// import About from "./pages/About/About";
 
 function App() {
   return (
     <>
-      {/* <Home /> */}
-      {/* <About/> */}
-
-      {/* <Navbar />
-      <CarouselSlider /> */}
-      {/* <MailForm /> */}
       <Form />
     </>
   );
