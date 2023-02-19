42ff161c7d3fc9c270beb2c8d323a7287c6bd861
kotesh-arya <koteswarraomudila@gmail.com>
2023-02-19 22:06:54 +0530

    FIX: Removed unused class

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,10 +6,7 @@ function Footer() {
     <div className="footer w-full p-2 mt-28 flex flex-col items-center bg-red-500">
       <h2 className="font-bold  ">Follow Us on </h2>
       <div className=" flex flex-row justify-between h-10 items-center w-1/6   ">
-        <img
-          m-0
-          src="https://img.icons8.com/ios-filled/30/null/facebook-new.png"
-        />
+        <img src="https://img.icons8.com/ios-filled/30/null/facebook-new.png" />
         <img src="https://img.icons8.com/ios-glyphs/30/null/instagram-new.png" />
         <img src="https://img.icons8.com/ios-glyphs/30/null/twitter--v1.png" />
       </div>
